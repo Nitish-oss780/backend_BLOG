cf0025eededf07a7a6cf026ae8584c25ecd5296f
Nitish-oss780 <75284890+Nitish-oss780@users.noreply.github.com>
2024-01-28 15:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
     method:["GET","POST","PUT","DELETE"],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true,
 }))
 // Connect to MongoDB
